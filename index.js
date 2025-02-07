5a6d8e6ff698a81949ad38c6721534fcc23b4573
David J. Hwang <jeongjaehwang@gmail.com>
2025-02-07 14:24:20 +0900

    Add index.css and index.js file to the project with buttons

---
+++ index.js
@@ -0,0 +1,6 @@
+const clickMeButton = document.querySelector('.click-me');
+const resultParagraph = document.querySelector('.result');
+
+clickMeButton.addEventListener('click', () => {
+window.alert("Button Clicked!")
+})
