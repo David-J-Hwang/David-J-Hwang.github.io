84a18a773bc65eb2b7dca7872f8b66f16d43b2c9
David-J-Hwang <jeongjaehwang@gmail.com>
2025-02-25 08:31:23 +0900

    Add tailwindcss to the client folder

--- index.js
+++
@@ -1,6 +0,0 @@
-const clickMeButton = document.querySelector('.click-me');
-const resultParagraph = document.querySelector('.result');
-
-clickMeButton.addEventListener('click', () => {
-  console.log("Button Clicked!")
-})
