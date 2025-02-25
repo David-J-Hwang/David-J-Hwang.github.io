e552bc68f0cd7e9dfef9ff8a70ec226b41e82c74
David-J-Hwang <jeongjaehwang@gmail.com>
2025-02-26 03:37:29 +0900

    Adjust the layout of Home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,19 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50 h-16 flex items-center">
+    <nav className="fixed top-0 left-0 w-full bg-blue-600 shadow-md z-50 h-16 flex items-center">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full flex justify-between items-center">
         {/* 로고 */}
-        <NavLink to="/" className="text-2xl font-bold text-blue-600">
+        <NavLink to="/" className="text-2xl font-bold text-stone-50">
           MyLogo
         </NavLink>
 
         {/* 네비게이션 버튼들 */}
-        <div className="space-x-4">
-          <NavLink to="/blogs" className="text-gray-700 hover:text-blue-500">
+        <div className="space-x-6">
+          <NavLink to="/blogs" className="text-gray-100 hover:text-white">
             Blogs
           </NavLink>
-          <NavLink to="/projects" className="text-gray-700 hover:text-blue-500">
+          <NavLink to="/projects" className="text-gray-100 hover:text-white">
             Projects
           </NavLink>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,11 @@ import DigitalClock from "../components/DigitalClock"
 
 function Home() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-50">
+    <div className="flex flex-col justify-center items-center min-h-screen gap-4 bg-gray-50">
       <DigitalClock />
 
-      <div className="flex justify-center gap-4 mt-8">
-        {/* <Weather />
-        <Calender /> */}
-      </div>
+        {/* <Weather /> */}
+        {/* <Calender /> */}
     </div>
   )
 }
