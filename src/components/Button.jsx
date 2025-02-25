636582000b6d63c1dee6801f7b742b5bcbfe014d
David-J-Hwang <jeongjaehwang@gmail.com>
2025-02-26 04:29:58 +0900

    Add width property to Button.jsx component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function Button({to, children}) {
+function Button({ to, children }) {
   return (
     <Link
       to={to}
-      className="px-6 py-3 bg-blue-500 text-white font-bold rounded-lg shadow-md hover:bg-blue-600 transition duration-300"
+      className="px-6 py-3 w-3xs bg-blue-500 text-center text-white font-bold rounded-lg shadow-md hover:bg-blue-600 transition duration-150"
     >
       {children}
     </Link>
-  )
+  );
 }
 
 export default Button;
