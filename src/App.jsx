1d32454b1ff7ad2dc0c3494258f8c41f5aa483f2
David-J-Hwang <jeongjaehwang@gmail.com>
2025-02-26 03:48:30 +0900

    Add a Get Weather button in Projects.jsx page that navigates to /projects/get-weather page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Navbar from "./components/Navbar";
 import Blogs from "./pages/Blogs";
 import Projects from './pages/Projects'
+import Weather from "./components/Weather";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path='/projects/get-weather' element={<Weather />} />
         </Routes>
         </div>
       </BrowserRouter>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,11 @@
+import Button from "../components/Button"
+
+
 function Projects() {
   return (
     <div>
       <h1 className="text-6xl text-center py-18">This is Projects page!</h1>
+      <Button to='/projects/get-weather'>Get Weather</Button>
     </div>
   )
 }
