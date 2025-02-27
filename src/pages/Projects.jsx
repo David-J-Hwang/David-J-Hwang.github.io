14b9e7e682afa36521d72ec899b6aa32b1bb3908
David J. Hwang <jeongjaehwang@gmail.com>
2025-02-28 06:52:15 +0900

    Add analog-clock and digital-clock link buttons to Projects.jsx page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,8 +4,14 @@ function Projects() {
   return (
     <div>
       <h1 className="text-6xl text-center pt-24">This is Projects page!</h1>
-      <div className="mt-18 flex flex-col items-center justify-center">
+      <div className="mt-18 flex flex-col items-center justify-center gap-4">
         <Button to="/projects/get-weather">Get Weather</Button>
+        <Button to="https://david-j-hwang.github.io/analog-clock">
+          Analog Clock
+        </Button>
+        <Button to="https://david-j-hwang.github.io/digital-clock">
+          Digital Clock
+        </Button>
       </div>
     </div>
   );
