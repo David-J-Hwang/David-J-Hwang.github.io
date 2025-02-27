d6258f3abf57a8e70ce26bbd91e3a87df5cf311d
David J. Hwang <jeongjaehwang@gmail.com>
2025-02-27 14:03:18 +0900

    Add Youtube and Naver Link buttons to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,22 @@
-import DigitalClock from "../components/DigitalClock"
+import DigitalClock from "../components/DigitalClock";
+import Button from "../components/Button";
 
 function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen gap-4 bg-gray-50">
       <DigitalClock />
 
-        {/* <Weather /> */}
-        {/* <Calender /> */}
+      <div className="mt-8 flex flex-row justify-center items-center gap-4">
+        <Button to={"https://youtube.com"}>Youtube</Button>
+        <Button to="https://naver.com">Naver</Button>
+        <Button to="https://map.naver.com">Naver Maps</Button>
+        <Button to="https://land.naver.com">Naver Real Estate</Button>
+      </div>
+
+      {/* <Weather /> */}
+      {/* <Calender /> */}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
